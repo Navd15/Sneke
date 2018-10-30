0c73102093e528b404dc4eed56d4d07a3d1cc5e0
Navd15 <gill.222navdeep@gmail.com>
2018-10-30 23:57:25 +0530

    main fragment class

---
+++ Sneke/src/main/java/sneke/nav/Sneke/Replacer.java
@@ -0,0 +1,34 @@
+package sneke.nav.Sneke;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Replacer extends Fragment {
+    public Replacer() {
+
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.grid,container,false);
+    }
+
+    @Override
+    public void onAttachFragment(Fragment fragment) {
+        super.onAttachFragment(fragment);
+    }
+}
+
